0777c63bdc649b8e11065fd311f9737b7223016d
sweetxia <2519646369@qq.com>
2022-02-12 14:57:59 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 全局样式
+import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 动态设置 REM 基准值
+import 'amfe-flexible'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
